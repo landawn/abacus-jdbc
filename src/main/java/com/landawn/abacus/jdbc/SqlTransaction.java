632e9af643794ab7d23b3a9663fae555ef603fac
haiyangl <lihy70@hotamil.com>
2026-05-25 00:18:21 -0700

    aaa

--- src/main/java/com/landawn/abacus/jdbc/SqlTransaction.java
+++ src/main/java/com/landawn/abacus/jdbc/SqlTransaction.java
@@ -725,7 +725,6 @@ public final class SqlTransaction implements Transaction, AutoCloseable {
      * @param creator the transaction creator type, must not be {@code null}
      * @return a unique transaction identifier string, never {@code null}
      */
-    @SuppressWarnings("deprecation")
     static String getTransactionId(final javax.sql.DataSource ds, final CreatedBy creator) {
         return Strings.concat(System.identityHashCode(ds), "_", Thread.currentThread().getId(), "_", creator.ordinal());
     }
